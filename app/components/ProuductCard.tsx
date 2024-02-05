6a6fb52ddf5c063c40e5ed3522690ce6e6ecfb8c
lorraineC26 <lorraine.quinyee@gmail.com>
2024-02-05 13:59:08 -0500

    create css.module for PC

--- app/components/ProuductCard.tsx
+++ app/components/ProuductCard.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import AddToCart from './AddToCart';
+import styles from './ProductCard.module.css'; // imported as an obj
+
 
 const ProuductCard = () => {
   return (
-    <div>
+    <div className={styles.card}>
       <AddToCart />
     </div>
 
