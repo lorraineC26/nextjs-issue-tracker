5850a45d0847626f2e16d83dcdf54b494879a060
lorraineC26 <lorraine.quinyee@gmail.com>
2024-02-05 14:13:00 -0500

    switch to tailwind; install daisyUI

--- app/components/ProuductCard.tsx
+++ app/components/ProuductCard.tsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import AddToCart from './AddToCart';
-import styles from './ProductCard.module.css'; // imported as an obj
+// import styles from './ProductCard.module.css'; // imported as an obj
 
 
 const ProuductCard = () => {
   return (
-    <div className={styles.card}>
+    // <div className={styles.card}>
+    //   <AddToCart />
+    // </div>
+
+    //using tailwind css
+    <div className='p-5 my-5 bg-sky-400 text-white text-xl hover:bg-sky-500'>
       <AddToCart />
     </div>
 
