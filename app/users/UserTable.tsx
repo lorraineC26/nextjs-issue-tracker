87947bcf975c516aaf767134452bf9baf37a9f1f
lorraineC26 <lorraine.quinyee@gmail.com>
2024-02-09 01:16:38 -0500

    set sorting feature by searchParams

--- app/users/UserTable.tsx
+++ app/users/UserTable.tsx
@@ -1,31 +1,51 @@
-import React from 'react'
+import React from "react";
+import { sort } from "fast-sort";
+import Link from "next/link";
 
 // define the shape of the user objects
 interface User {
   id: number;
   name: string;
-  email:string;
+  email: string;
 }
 
-const UserTable = async() => {
-  const res = await fetch('https://jsonplaceholder.typicode.com/users', {cache: 'no-store'}); //disable caching
+interface Props {
+  sortOrder: string;
+}
+
+const UserTable = async ({ sortOrder }: Props) => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users", {
+    cache: "no-store",
+  }); //disable caching
   const users: User[] = await res.json(); // annotate the type as a custom User array
 
-  return (
-    <table className='table table-bordered'>
-        <thead>
-          <tr>
-            <th>Name</th> 
-            <th>Email</th>
-          </tr>
-        </thead>
-        <tbody>
-          {users.map(user => <tr key={user.id}>
-            <td>{user.name}</td>
-            <td>{user.email}</td></tr>)}
-        </tbody>
-    </table>
-  )
-}
+  // sort users (array of objects) by name in ascending order by default
+  const sortedUsers = sort(users).asc(
+    sortOrder === "email" ? (user) => user.email : (user) => user.name
+  );
 
-export default UserTable
+  return (
+    <table className="table table-bordered">
+      <thead>
+        <tr>
+          <th>
+            <Link href="/users?sortOrder=name">Name</Link>
+          </th>
+          <th>
+            <Link href="/users?sortOrder=email">Email</Link>
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        {sortedUsers.map((user) => (
+          <tr key={user.id}>
+            <td>{user.name}</td>
+            <td>{user.email}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+export default UserTable;
