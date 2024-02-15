f09d417eb2db458ddab01c0bfb873c35b9cfe57b
lorraineC26 <lorraine.quinyee@gmail.com>
2024-02-15 16:26:57 -0500

    add comments to error.tsx

--- app/error.tsx
+++ app/error.tsx
@@ -3,18 +3,22 @@ import React from 'react'
 
 interface Props {
   error: Error;
-  reset: () => void;
+  reset: () => void; // has to be a function
 }
 
 const ErrorPage = ( {error, reset}: Props) => {
-  console.log('Error', error)
+  // in real world we want to log this error not only in console but also somewhere else so we can take actions
+  // Mosh suggests https://sentry.io/welcome/
+  console.log("Error", error);
 
   return (
     <>
       <div>An unexpected error has occured.</div>
-      <button className='btn' onClick={() => reset()}>Retry</button>
+      <button className="btn" onClick={() => reset()}>
+        Retry
+      </button>
     </>
-  )
+  );
 }
 
 export default ErrorPage
