b02f88c74316c452b660df831f6e9fde00ef9da1
lorraineC26 <lorraine.quinyee@gmail.com>
2024-02-15 14:45:42 -0500

    add useRouter for create button

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -3,6 +3,7 @@
 
 import React from 'react'
 import UserTable from './UserTable';
+import Link from 'next/link';
 
 interface Props {
   searchParams: {sortOrder: string}; // query string in the browser address 
@@ -13,6 +14,7 @@ const UsersPage = async({searchParams: {sortOrder}}: Props) => {
   return (
     <>
       <h1>Users</h1>
+      <Link href="/users/new" className='btn'> NEW USER </Link>
       <UserTable sortOrder={sortOrder}/>
     </>
   )
