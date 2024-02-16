6b3835b4160b90c6e46a5a92fff88511b51af6dd
lorraineC26 <lorraine.quinyee@gmail.com>
2024-02-16 15:08:28 -0500

    add hardcode data for user/id res

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -1,4 +1,5 @@
-import { NextRequest } from "next/server";
+import { error } from "console";
+import { NextRequest, NextResponse } from "next/server";
 
 // interface Props {
 //   params: { id: number };
@@ -18,7 +19,10 @@ export function GET(
   // if not found, return 404 error
   // else return data
 
-  
+  if (params.id > 10) {
+    return NextResponse.json({error: 'User not found'}, {status: 404})
+  }
 
+  return NextResponse.json({id: 1, name: 'Lorraine'})
 
 }
