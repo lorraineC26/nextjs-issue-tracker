87904bd58625cdd0fa7374a62d49f828fc13ec13
lorraineC26 <lorraine.quinyee@gmail.com>
2024-02-16 15:35:33 -0500

    add POST req to users

--- app/api/users/route.tsx
+++ app/api/users/route.tsx
@@ -1,10 +1,18 @@
 import { NextRequest, NextResponse } from "next/server";
 
-export function GET(request: NextRequest) { // `request` params here is to prevent caching
+export function GET(request: NextRequest) { // thought `request` params here is not used, but we have to have it to prevent caching
   // in real world: fetch usres from a db
 
   return NextResponse.json([
     {id: 1, name: 'Lorraine'},
     {id: 2, name: 'Qinyi'},
   ]);
+}
+
+export async function POST(request: NextRequest) {
+  const body = await request.json();
+  //Validate
+  // if invalid, return 400
+  // else, return data
+  return NextResponse.json({id: 1, name: body.name});
 }
