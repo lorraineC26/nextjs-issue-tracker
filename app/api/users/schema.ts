e0969b18a5a282a98d0ca0d9626a3e1d9311f8d9
lorraineC26 <lorraine.quinyee@gmail.com>
2024-02-24 20:23:41 -0500

    create POST and GET fx for api/products

--- app/api/users/schema.ts
+++ app/api/users/schema.ts
@@ -1,6 +1,7 @@
 import {z} from 'zod';
 
 // define the user object and use zod to validate objects
+// don't need to include id when defining
 const schema =  z.object({
   name: z.string().min(3), // name with a min length of 3
   // email: z.string().email(),
